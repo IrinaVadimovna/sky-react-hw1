303cc113ca79410e778e8b7b586a8e7f6195c671
IrinaVadimovna <101879008+IrinaVadimovna@users.noreply.github.com>
2022-05-26 10:40:55 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Book from './Book';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Book name="JS for beginners" year="2018" price="1000" />
+        <Book name="React for beginners" year="2019" price="1200" />
+        <Book name="Vue for beginners" year="2021" price="1500" />
+      </div>
+      <div>
+        <h1>Hello from React</h1>
+        <div>
+          <Book name="JS for beginners" year="2018" price="1000" />
+          <Book name="React for beginners" year="2019" price="1200" />
+          <Book name="Vue for beginners" year="2021" price="1500" />
+        </div>
+      </div>
     </div>
   );
 }
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
+  rootElement,
+);
 
 export default App;
