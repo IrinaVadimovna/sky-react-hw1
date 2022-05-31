d24bd2254b480bff9842b3e4c5782732ee84fefd
IrinaVadimovna <101879008+IrinaVadimovna@users.noreply.github.com>
2022-05-31 13:28:42 +0300

    Add files via upload

---
+++ MinMax.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const min = 0;
+const max = 10;
+
+class MinMax extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { curCount: min };
+  }
+
+  inc = () => {
+    if (this.state.curCount >= max) {
+      this.state.curCount = max - 1;
+    }
+    this.setState((prevState) => ({ curCount: prevState.curCount + 1 }));
+  };
+
+  minus = () => {
+    if (this.state.curCount <= min) {
+      this.state.curCount = min + 1;
+    }
+    this.setState((nextState) => ({ curCount: nextState.curCount - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.minus}>
+          {" "}
+          -{" "}
+        </button>
+        <input type="number" value={this.state.curCount}/>
+        <button type="button" onClick={this.inc}>
+          {" "}
+          +{" "}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default MinMax;
